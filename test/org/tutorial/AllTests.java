f4c038acb1aadf801dd2ffccf56e209802e241b1
mattsmart <mattsmart@hotmail.ca>
2013-11-17 01:36:39 -0500

    AllTests

---
+++ test/org/tutorial/AllTests.java
@@ -0,0 +1,11 @@
+package org.tutorial;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BookTest.class, HumanTest.class })
+public class AllTests {
+
+}
